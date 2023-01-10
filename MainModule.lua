a2ed6216f3cebeb1546f9622fdd07938765cabeb
DarkDrake666 <97185244+DarkDrake666@users.noreply.github.com>
2023-01-10 17:47:44 +0000

    Update MainModule.lua

--- MainModule.lua
+++ MainModule.lua
@@ -1,4 +1,22 @@
+--[[
+local Settings = {
+    BlockScriptLogging = true,
+    HideUsernameFromScripts = true,
+    BlockScriptKicks = true,
+    BlockGameKicks = true,
+    BlockScriptChatRequests = true,
+    
+    -- BROKEN
+    
+    ConfirmScriptFileMaking = false, -- this could break a little amount of scripts as it uses a function that yields.
+    ConfirmScriptFileDeleting = false, -- this could break a little amount of scripts as it uses a function that yields.
+    ConfirmScriptFileAppending = false -- this could break a little amount of scripts as it uses a function that yields.
+}
+]]--
+
 local v1 = {}
+
+local LP = game:GetService("Players").LocalPlayer
 
 local Notification = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
@@ -21,7 +39,7 @@ local AlreadyLoaded = {
     resume = function() MakeNotification("Error", "BetterMadeSecurity is already loaded.", 10) end
 }
 
-if not loadstring or not getgenv() or not hookfunction or not hookmetamethod or not getnamecallmethod then
+if not loadstring or not getgenv() or not hookfunction or not hookmetamethod or not getnamecallmethod or not setreadonly or not isreadonly or not newcclosure then
     return NotSupported
 end
 
@@ -37,30 +55,39 @@ v1.fire = function(t1)
     if ChatEvent.Parent ~= game:GetService("ReplicatedStorage"):FindFirstChild("DefaultChatSystemChatEvents") then
         ChatEvent = game:GetService("ReplicatedStorage"):FindFirstChild("DefaultChatSystemChatEvents"):FindFirstChild("SayMessageRequest")
     end
-    if not ChatEvent and (t1.BlockScriptChatRequests and t1.BlockScriptChatRequests == true) then
+    if not ChatEvent and ((t1.BlockScriptChatRequests and t1.BlockScriptChatRequests == true) or (t1.BlockGameChatRequests and t1.BlockGameChatRequests == true)) then
         MakeNotification("Error", "Chat remotes not found. This most likely means chat is disabled.")
     end
+    
     if t1.BlockScriptLogging and t1.BlockScriptLogging == true then
-        local SEND = syn and syn.request or http and http.request or fluxus and fluxus.request or http_request or request
-        local Old; Old = hookfunction(SEND, newcclosure(function(...)
-            if stopped == true then
-                return Old(...)
+        local SEND = syn or http or fluxus
+        if SEND then
+            local Old = syn.request
+            
+            setreadonly(SEND, false);
+            SEND.request = function(data)
+                local Url = data.Url
+                local Method = data.Method
+                
+                if stopped == true then
+                    return Old(data)
+                end
+                if not data.Method then
+                    return Old(data) -- default is set to "GET"
+                end
+                
+                if Method == "POST" then
+                    print("Your script sent a logging request to: \n"..Url.."\nUsing "..Method.."\n")
+                    MakeNotification("Logging Attempt", "Your script attempted to log you! URL has been printed.")
+                    return nil
+                end
+                return Old(data)
             end
-            if not checkcaller() then
-                return Old(...) -- wtf game sending syn.request
-            end
-            local args = {...}
-            if not args.Method then
-                return Old(...) -- default is set to "GET"
-            end
-            if args.Method == "POST" or args.Method == "SEND" then
-                print("Your script sent a logging request to: \n"..args.Url.."\nUsing "..args.Method)
-                MakeNotification("Logging Attempt", "Your script attempted to log you! URL has been printed.")
-                return nil
-            end
-            return Old(...)
-        end))
+        else
+            MakeNotification("BetterMadeSecurity", "Unfortunately, StopScriptLogging doesn't support your Executor. Error: Request Functions MUST be stored in a table.")
+        end
     end
+    
     if t1.HideUsernameFromScripts and t1.HideUsernameFromScripts == true then
         local Old; Old = hookmetamethod(game, "__index", newcclosure(function(Self, Key)
             if stopped == true then
@@ -69,40 +96,46 @@ v1.fire = function(t1)
             if not checkcaller() then
                 return Old(Self, Key)
             end
-            local LocalPlayer = game:GetService("Players").LocalPlayer
-            if Self == LocalPlayer and Key == "Name" then
+            if Self == LP and Key == "Name" then
                 MakeNotification("Name Attempt", "Your script attempted to get your Username. Returned nil")
                 return "John Doe"
-            elseif Self == LocalPlayer and Key == "DisplayName" then
+            elseif Self == LP and Key == "DisplayName" then
                 MakeNotification("DisplayName Attempt", "Your script attempted to get your DisplayName. Returned nil")
                 return "John Doe"
-            elseif Self == LocalPlayer and Key == "UserId" then
+            elseif Self == LP and Key == "UserId" then
                 MakeNotification("UserId Attempt", "Your script attempted to get your UserId. Returned nil")
                 return "2"
-            elseif Self == LocalPlayer and Key == "Username" then
+            elseif Self == LP and Key == "Username" then
                 MakeNotification("Name Attempt", "Your script attempted to get your Username. Returned nil")
                 return "John Doe"
             end
             return Old(Self, Key)
         end))
     end
-    local Old; Old = hookmetamethod(game, "__namecall", newcclosure(function(...)
-        local args = {...}
-        Self = args[1]
+    local Old; Old = hookmetamethod(game, "__namecall", newcclosure(function(Self, ...)
         if stopped == true then
-            return Old(...)
+            return Old(Self, ...)
         end
+        local CallingScript = getcallingscript() or getfenv(0)["script"]
         local NCM = string.lower(tostring(getnamecallmethod()))
-        local LocalPlayer = game:GetService("Players").LocalPlayer
-        if checkcaller() and Self == ChatEvent and (t1.BlockScriptChatRequests and t1.BlockScriptChatRequests == true) and NCM == "fireserver" then
-            print("Your script sent a logging request to: \n"..args.Url.."\nUsing "..args.Method)
-            MakeNotification("Chat Attempt", "Your script attempted to make a chat request. Message has been printed.")
+        local args = {...}
+        if Self == ChatEvent and ((t1.BlockScriptChatRequests and t1.BlockScriptChatRequests == true) or (t1.BlockGameChatRequests and t1.BlockGameChatRequests == true)) and NCM == "fireserver" then
+            if checkcaller() then
+                print("Your script sent a chat request with the message: \n"..args[1].."\nTo "..args[2].."\n")
+                MakeNotification("Script Chat Attempt", "Your script attempted to make a chat request. Message has been printed.")
+                return nil
+            end
+            return Old(Self, ...)
         end
-        if Self == LocalPlayer and NCM == "kick" then
+        if Self == LP and NCM == "kick" then
             if not checkcaller() and (t1.BlockGameKicks and t1.BlockGameKicks == true) then
+                print("Your game attempted to make a kick request: \n"..tostring(args[1]).."\n")
+                MakeNotification("Game Kick Attempt", "Your game attempted to make a kick request. Message has been printed.")
                 return nil
             end
             if checkcaller() and (t1.BlockScriptKicks and t1.BlockScriptKicks == true) then
+                print("Your script attempted to make a kick request: \n"..tostring(args[1]).."\n")
+                MakeNotification("Script Kick Attempt", "Your script attempted to make a kick request. Message has been printed.")
                 return nil
             end
         elseif Self == game and not checkcaller() and (t1.BlockScriptLogging and t1.BlockScriptLogging == true) == true then
@@ -111,7 +144,7 @@ v1.fire = function(t1)
                 return nil
             end
         end
-        return Old(...)
+        return Old(Self, ...)
     end))
     if t1.ConfirmScriptFileMaking and t1.ConfirmScriptFileMaking == true then
         local Old; Old = hookfunction(makefolder, newcclosure(function(...)
@@ -199,8 +232,8 @@ v1.fire = function(t1)
             return Old(...)
         end))
     end
-    print("BetterMadeSecurity successfully initialised!")
-    print("Your current settings are: \nBlockScriptLogging: "..(t1.BlockScriptLogging and t1.BlockScriptLogging == true)..", \nHideUsernameFromScripts: "..(t1.HideUsernameFromScripts and t1.HideUsernameFromScripts == true)..", \nBlockScriptKicks: "..(t1.BlockScriptKicks and t1.BlockScriptKicks == true)..", \nBlockGameKicks: "..(t1.BlockGameKicks and t1.BlockGameKicks == true)..", \nBlockScriptChatRequests: "..(t1.BlockScriptChatRequests and t1.BlockScriptChatRequests == true)..", \nConfirmScriptFileMaking: "..(t1.ConfirmScriptFileMaking and t1.ConfirmScriptFileMaking == true)..", \nConfirmScriptFileDeleting: "..(t1.ConfirmScriptFileDeleting and t1.ConfirmScriptFileDeleting == true)..", \nConfirmScriptFileAppending: "..(t1.ConfirmScriptFileAppending and t1.ConfirmScriptFileAppending == true).."\n")
+    print("BetterMadeSecurity successfully initialised! \n")
+    print("Your current settings are: \nBlockScriptLogging: "..tostring(t1.BlockScriptLogging)..", \nHideUsernameFromScripts: "..tostring(t1.HideUsernameFromScripts)..", \nBlockScriptKicks: "..tostring(t1.BlockScriptKicks)..", \nBlockGameKicks: "..tostring(t1.BlockGameKicks)..", \nBlockScriptChatRequests: "..tostring(t1.BlockScriptChatRequests)..", \nConfirmScriptFileMaking: "..tostring(t1.ConfirmScriptFileMaking)..", \nConfirmScriptFileDeleting: "..tostring(t1.ConfirmScriptFileDeleting)..", \nConfirmScriptFileAppending: "..tostring(t1.ConfirmScriptFileAppending).."\n")
 end
 
 v1.stop = function()
@@ -211,6 +244,4 @@ v1.resume = function()
     getgenv().stopped = false
 end
 
-print("BetterMadeSecurity successfully loaded!")
-
-return v1
+return v1;
